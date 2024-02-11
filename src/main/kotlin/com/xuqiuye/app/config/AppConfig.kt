4f1421ecc70e9eff2bb2b0e8829407144af4996e
Xuqiuye Li <xuqiuye.li@gmail.com>
2024-02-11 18:31:26 +0900

    add security config to allow authorized access to Http calls

--- src/main/kotlin/com/xuqiuye/app/config/AppConfig.kt
+++ src/main/kotlin/com/xuqiuye/app/config/AppConfig.kt
@@ -1,13 +1,20 @@
 package com.xuqiuye.app.config
 
 import com.xuqiuye.app.repository.UserRepository
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
+@EnableConfigurationProperties(JwtProperties::class)
 class AppConfig(
     private val userRepository: UserRepository
 ) {
@@ -18,5 +25,26 @@ class AppConfig(
             userRepository.findUserByEmail(username)
                 ?: throw UsernameNotFoundException("User not found")
         }
+    }
+
+    @Bean
+    fun authenticationProvider(userRepository: UserRepository): AuthenticationProvider {
+        val authenticationProvider = DaoAuthenticationProvider()
+        authenticationProvider
+            .also {
+                it.setUserDetailsService(userDetailsService())
+                it.setPasswordEncoder(passwordEncoder())
+            }
+        return authenticationProvider
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager {
+        return config.authenticationManager
     }
 }
